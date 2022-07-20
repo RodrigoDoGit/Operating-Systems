754279daed2a693ab31f9ea1a478ef34bb858dd5
Rodrigo Emídio <94897828+RodrigoDoGit@users.noreply.github.com>
2022-07-20 19:48:07 +0100

    Add files via upload

---
+++ Código Fonte/Folha 1/Q3/pointers1.c
@@ -0,0 +1,13 @@
+int main() {
+   int i, j, *p, *q;
+   i = 5;
+   p = &i;
+  *p = 7;
+   j = 3;
+   p = &j;
+   q = p;
+   p = &i;
+  *q = 2;
+   return 0;
+}
+//printf("i=%d, j=%d, p=%p, q=%p\n", i, j, p, q);
